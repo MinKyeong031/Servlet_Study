ba6431f5546a55a87900ad53dd7a5b664da651c6
MinKyeong031 <s2019s07@e-mirim.hs.kr>
2021-05-10 12:56:12 +0900

    210506

---
+++ ServletPrj/src/kr/hs/servlet/SecondServlet.java
@@ -0,0 +1,27 @@
+package kr.hs.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/oddeven")
+public class SecondServlet extends HttpServlet{
+	@Override
+	public void init() throws ServletException {
+		System.out.println("init() ¸Þ¼­µå");
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int a = 3;
+		if(a%2 == 0) {
+			System.out.println("Â¦");
+		} else {
+			System.out.println("È¦");
+		}
+	}
+}
